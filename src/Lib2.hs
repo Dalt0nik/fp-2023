56aad43b4971a836fcea6100730dda2398324018
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2023-10-26 19:41:35 +0300

    implement show table name

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -10,6 +10,7 @@ where
 import DataFrame (DataFrame (..), Value (..), Column (..), ColumnType (..))
 import InMemoryTables (TableName, database)
 import Data.Char (toLower)
+import Data.List (isPrefixOf)
 
 type ErrorMessage = String
 type Database = [(TableName, DataFrame)]
@@ -37,14 +38,16 @@ data LogicalOp
 data ParsedStatement
   = ShowTablesStatement
   | ShowTableStatement TableName
-  | SelectStatement [Column] TableName [Condition]
+  | SelectStatement [Column] TableName Condition
 
 -- Parses user input into an entity representing a parsed
 -- statement
 parseStatement :: String -> Either ErrorMessage ParsedStatement
 parseStatement input
   | map toLower input == "show tables" = Right ShowTablesStatement
-  -- | -- SHOW TABLE name parsing
+  | "show table" `isPrefixOf` map toLower input =
+    let tableName = drop 11  (map toLower input)
+      in Right  (ShowTableStatement tableName)
   -- | -- SELECT parsing
   | otherwise = Left "Not implemented: parseStatement"
 
@@ -52,11 +55,17 @@ parseStatement input
 -- Executes a parsed statemet. Produces a DataFrame. Uses
 -- InMemoryTables.databases a source of data.
 executeStatement :: ParsedStatement -> Either ErrorMessage DataFrame
-executeStatement ShowTablesStatement = Right $ DataFrame [Column "Table Name" StringType] (map (\tableName -> [StringValue tableName]) (showTables database))
+executeStatement ShowTablesStatement = Right $ DataFrame [Column "TABLE NAME" StringType] (map (\tableName -> [StringValue tableName]) (showTables database))
+executeStatement (ShowTableStatement tableName) =
+  case lookup (map toLower tableName) database of
+    Just (DataFrame columns _) -> Right $ DataFrame [Column "COLUMN NAMES" StringType] (map (\col -> [StringValue (extractColumnName col)]) columns)
+    Nothing -> Left (tableName ++ " not found")
 -- | -- SelectStatement implementation
--- | -- ShowTableStatement implementation
 executeStatement _ = Left "Not implemented: executeStatement"
 
 --used in ShowTables execution
 showTables :: Database -> [TableName]
 showTables db = map fst db
+
+extractColumnName :: Column -> String
+extractColumnName (Column name _) = name
